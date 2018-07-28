6cd2c6d9c400a1e6eee0483d354edbe368331312
annapismenna <pismennaanna@gmail.com>
2018-07-28 19:44:10 +0300

    fix sum function

--- javascript/1/sum.js
+++ javascript/1/sum.js
@@ -1,9 +1,12 @@
 function sum(num) {
-    var result = 0;
+    let result = 0;
 
     function f(num){
-        if(num) {
-            result += num;
+        const _num = +num;
+        if (_num === _num){
+            result += _num;
+            return f;
+        } else if (num !== undefined){
             return f;
         } else {
             return result;
